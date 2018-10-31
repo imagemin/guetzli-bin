8f08032a519bb91ea08747d7e5855003e2d8923b
1000ch <shogo.sensui@gmail.com>
2018-10-31 19:01:56 +0900

    Require Node.js 6

--- test/test.js
+++ test/test.js
@@ -4,23 +4,20 @@ const test = require('ava');
 const execa = require('execa');
 const tempy = require('tempy');
 const binCheck = require('bin-check');
-const BinBuild = require('bin-build');
+const binBuild = require('bin-build');
 const compareSize = require('compare-size');
 const executable = require('executable');
 const guetzli = require('..');
 
-test.cb('rebuild the guetzli binaries', t => {
+test('rebuild the guetzli binaries', async t => {
 	const tmp = tempy.directory();
 
-	new BinBuild()
-		.src('https://github.com/google/guetzli/archive/v1.0.1.tar.gz')
-		.cmd(`mkdir -p ${tmp}`)
-		.cmd(`make && mv bin/Release/guetzli ${tmp}`)
-		.run(err => {
-			t.ifError(err);
-			t.true(executable.sync(path.join(tmp, 'guetzli')));
-			t.end();
-		});
+	await binBuild.url('https://github.com/google/guetzli/archive/v1.0.1.tar.gz', [
+		`mkdir -p ${tmp}`,
+		`make && mv bin/Release/guetzli ${tmp}`
+	]);
+
+	t.true(executable.sync(path.join(tmp, 'guetzli')));
 });
 
 test('return path to binary and verify that it is working', async t => {
